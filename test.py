d9b55649319911a5183dc750234c4f6286e857bc
rubenvas <ruben.vasconcelos@mozobi.com>
2016-06-27 09:51:23 +0100

    added unit tests

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print 'test'
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertFalse('Foo'.isupper())
+
+if __name__ == '__main__':
+    unittest.main()
